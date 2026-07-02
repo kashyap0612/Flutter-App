28f83a4bd6acbbd0e9f729f8d1df795c3c1431a5
Aditya Anand Kashyap <kashyap10115@example.com>
2026-07-02 19:19:42 +0530

    Backup local Flutter project restructuring and files

--- lib/utils/formatters.dart
+++
@@ -1,6 +0,0 @@
-import 'package:intl/intl.dart';
-
-class Formatters {
-  static String dateTime(DateTime dt) => DateFormat('dd MMM yyyy, hh:mm a').format(dt);
-  static String percent(double value) => '${(value * 100).toStringAsFixed(2)}%';
-}
